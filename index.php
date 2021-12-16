e1bdf645ad61706096fdcc9e4789b54304e80034
ljdedic <ljiljana.dedic@fpmoz.sum.ba>
2021-12-16 14:32:29 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE-edge">
+        <title>Charlotte</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            <img class="logo" src="LogoMakr-68qNCA.png" alt="logo">
+            <nav>
+                <ul class="nav_links">
+                    <li><a href="">Naslovnica</a></li>
+                    <li><a href="">O nama</a></li>
+                    <li><a href="">Meni</a></li>
+                    <li><a href="">Kako naručiti</a></li>
+                    <li><a href="">Kontakt</a></li>
+                    <li><a href=""></a></li>
+                </ul>
+            </nav>
+            <nav>
+                <img class="slika1" src="preuzmi.png" alt="slika1">
+            </nav>
+        </header>
+        
+
+    </body>
+</html>
